d3e519a0c8c1f8f7898d820536a95e08b65aaea8
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2023-05-08 15:38:10 +0530

    Divided using grid

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,8 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Grid from "@mui/material/Grid";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      <Grid item xs={3}>
+        1
+      </Grid>
+      <Grid item xs={5}>
+        2
+      </Grid>
+      <Grid item xs={4}>
+        3
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
